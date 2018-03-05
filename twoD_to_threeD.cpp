de7350fc87b70133176ff5e7f5cfb236337e856b
Arshdeep Singh <arshdeepiitd@gmail.com>
2018-03-05 11:53:30 +0530

    added file

---
+++ twoD_to_threeD.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <"input.cpp">
+
+using namespace std;
+
+class twoD_to_threeD
+{
+public:
+
+	// @2D to 3D transformation
+	returntype find_correspondence() {
+
+	}
+
+	returntype generate_3D_edges(){
+
+	}
+
+	returntype generate_face() {
+
+	}
+
+	returntype generate_3D_object() {
+
+	}
+
+	returntype check_edges() {
+
+	}
+
+	returntype check_vertices() {
+
+	}	
+
+	returntype check_faces() {
+
+	}
+
+};
