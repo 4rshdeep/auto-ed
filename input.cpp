144d33595b79e06411a0be28e24ada9ba291968f
Arshdeep Singh <arshdeepiitd@gmail.com>
2018-03-05 11:11:10 +0530

    added file

---
+++ input.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class input
+{
+public:
+	/* selects mode */
+	int select_mode() {
+		return 1;
+	}
+
+	void get_input() {
+
+	}
+
+	void threeD_to_graph() {
+
+	}
+
+	void twoD_to_graph() {
+		
+	}
+
+};
