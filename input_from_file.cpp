4954e88d2c7273096f67599216a676263820ba26
Arshdeep Singh <arshdeepiitd@gmail.com>
2018-03-11 02:33:11 +0530

    stuff

--- input_from_file.cpp
+++ input_from_file.cpp
@@ -5,9 +5,27 @@
 
 using namespace std;
 
+struct coordinate
+{
+    double x;
+    double y;
+    double z;
+};
+
+struct node
+{
+    vector<node*> adj_list; // pointer to neighbours
+    coordinate coord;
+};
+
+
 int main() {
-    // float sum = 0;
-    float x;
+    double x;
+    double y;
+    double z;
+    int size;
+    
+
     ifstream inFile;
     
     inFile.open("input.txt");
@@ -17,31 +35,43 @@ int main() {
         // exit(1); // terminate with error
     }
 
-    int num;
-    inFile >> num;
-    cout << num;
-    x = num;
-    double data[num][3];
+    inFile >> size;
+    double data[size][3];
 
     int i=0;
     int j = 0;
 
+    std::vector<node> v;
+    
     while (inFile >> x) {
-        if (j%3==0)
-        {
-            i++;
-            j = 0;
-        }
-        data[i][j] = x;
-        // sum = sum + x;
-        cout << "Last Input = " << x << "  and   data[" << i << "][" << j << "] = " << data[i][j] << endl;
-        j++;    
+        inFile >> y;
+        inFile >> z;
+
+        node n;
+        n.coord.x = x;
+        n.coord.y = y;
+        n.coord.z = z;
+
+        // cout << n.coord.x << " " << n.coord.y << " " << n.coord.z << endl;
+        v.push_back(n);
+        // if (j%3==0)
+        // {
+        //     i++;
+        //     j = 0;
+        // }
+        // data[i][j] = x;
+        // // sum = sum + x;
+        // cout << "Last Input = " << x << "  and   data[" << i << "][" << j << "] = " << data[i][j] << endl;
+        // j++;    
     }
     
+    for (int i = 0; i < v.size(); ++i)
+    {
+        
+        node n = v[i];
+        cout << n.coord.x << " " << n.coord.y << " " << n.coord.z << endl;
+    }
 
-    // arma::mat arma_matrix( data, 6, 3, true, true );
-    // arma_matrix.print();
-    // cout << num;
 
     inFile.close();
     return 0;
