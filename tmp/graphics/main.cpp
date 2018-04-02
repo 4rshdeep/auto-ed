7aa96ac552050b110e9fe5c62d33caca9ac5cb9a
Arshdeep Singh <arshdeepiitd@gmail.com>
2018-04-02 14:27:43 +0530

    changes

--- tmp/graphics/main.cpp
+++ tmp/graphics/main.cpp
@@ -17,22 +17,31 @@ int main(int argc, char *argv[])
    QLabel l;
    QPicture pi;
    QPainter p(&pi);
-
+   
    p.setRenderHint(QPainter::Antialiasing);
-   p.setPen(QPen(Qt::black, 5, Qt::DashDotLine, Qt::RoundCap));
+   p.setPen(QPen(Qt::black, 5, Qt::SolidLine, Qt::RoundCap));
    // p.drawLine(0, 0, 200, 200);
    
-   float x, y, prev_x=0, prev_y=0 ;
+   // float x, y, prev_x=0, prev_y=0 ;
+   p.drawLine(0, 0, 700, 0);
+   p.drawLine(700, 0, 700, 700);
+   p.drawLine(700, 700, 0, 700);
+   p.drawLine(0, 700, 0, 0);
+   
+   p.drawLine(10, 10, 490, 10);
+   p.drawLine(490, 10, 490, 490);
+   p.drawLine(490, 490, 10, 490);
+   p.drawLine(10, 490, 10, 10);
 
-   for (int i = 1; i < SIZE; ++i){
-		x = i*STEP;
-		y = sin(x);
-      // y = x;
-		p.drawLine(FACTOR*prev_x, FACTOR*prev_y, FACTOR*x, FACTOR*y);
+ //   for (int i = 1; i < SIZE; ++i){
+	// 	x = i*STEP;
+	// 	y = sin(x);
+ //      // y = x;
+	// 	p.drawLine(FACTOR*prev_x, FACTOR*prev_y, FACTOR*x, FACTOR*y);
 
-		prev_x = x;
-		prev_y = y;
-	}
+	// 	prev_x = x;
+	// 	prev_y = y;
+	// }
 
    p.end(); // Don't forget this line!
 
