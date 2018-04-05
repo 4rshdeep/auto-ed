afda1780024bb7530f1edf3bda40a720de92efb3
Arshdeep Singh <arshdeepiitd@gmail.com>
2018-04-05 21:53:30 +0530

    documentation added

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,5 @@
-#include "mainwindow.h"
-// #include "ui_mainwindow.h"
+#include "include/mainwindow.h"
+// #include "include/ui_mainwindow.h"
 #include <ui_mainwindow.h>
 #include <bits/stdc++.h>
 #include <QtCore>
